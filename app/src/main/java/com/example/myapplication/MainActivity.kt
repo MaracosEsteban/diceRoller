dc3cdf103d7d4893b697b2e2e4e4271dfb56e2dd
MaracosEsteban <mculzoni@hotmail.com>
2023-03-14 20:18:44 +0100

    antes de migrar a androidX

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,32 +3,57 @@ package com.example.myapplication
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+        lateinit var diceImage : ImageView
+        diceImage = findViewById(R.id.dice_image)
+
+
+
+
+
         val rollButton: Button = findViewById(R.id.roll_button)
-        val resultText: TextView = findViewById(R.id.result_text)
-        val upButton:Button = findViewById(R.id.ButtonCountUp)
+        //The lateinit keyword promises the Kotlin compiler that the variable will be initialized before the code calls any operations on it.
+
 
         rollButton.setOnClickListener {
-            resultText.text = (1..6).random().toString()
 
+            val imRes = when ((1..6).random()) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+
+            diceImage.setImageResource(imRes)
         }
-
-
-        upButton.setOnClickListener {
-            resultText.text =
-                if (resultText.text.toString() == "ButtonCountUp") "ButtonCountUp" else {
-                    var num = resultText.text.toString().toInt()
-                    if (num < 6) num++
-                    num.toString()
-                }
-        }
-
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
