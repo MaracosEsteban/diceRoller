808c02abf281a0e84256b1a9fa1c83336b46f4de
MaracosEsteban <mculzoni@hotmail.com>
2023-03-15 12:40:19 +0100

    now are two dice

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,59 +1,43 @@
 package com.example.myapplication
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    //The lateinit keyword promises the Kotlin compiler that the variable will be initialized before the code calls any operations on it.
+    private lateinit var diceImage1: ImageView
+    private lateinit var diceImage2: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        lateinit var diceImage : ImageView
-        diceImage = findViewById(R.id.dice_image)
-
-
-
-
-
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
         val rollButton: Button = findViewById(R.id.roll_button)
-        //The lateinit keyword promises the Kotlin compiler that the variable will be initialized before the code calls any operations on it.
-
 
         rollButton.setOnClickListener {
-
-            val imRes = when ((1..6).random()) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> R.drawable.dice_6
-            }
-
-            diceImage.setImageResource(imRes)
+            diceImage1.setImageResource(rollDice())
+            diceImage2.setImageResource(rollDice())
         }
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* Esta función devuelve un número entero que es el id del recurso */
+    private fun rollDice(): Int {
+        val imRes = when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        return imRes
+    }
 
 }
